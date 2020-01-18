a676a038986194f0928b5c5e441fcd28786fef71
uniqly <simontong09@gmail.com>
2020-01-18 14:58:51 +0800

    2020_18_1_1500
    XD

--- src/ResultForm.js
+++ src/ResultForm.js
@@ -43,7 +43,7 @@ class ResultForm extends Component {
               Your drink contains { drinkCal } calories.<br/>
               Overall you have consumed { totalCal } calories.<br/>
               Your daily recommanded calorie intake based on your age and gender is { kcal } calories.<br/>
-              Your suggested calorie intake for this meal is { Math.round(kcal/2)}
+              Your suggested calorie intake for this meal is { Math.round(kcal/3) }
             </div>
           </div>
         </div>
--- src/forms/FoodForm.js
+++ src/forms/FoodForm.js
@@ -12,20 +12,43 @@ class FoodForm extends Component {
 						<div className="more">I want to eat...</div>
 						<select className="appBox foodBox" name="food" onChange={ this.props.handleInput }>
 							<option value="0">I'm not hungry</option>
-							<option value="370">Fishball noodles</option>
-							<option value="411">Wanton Noodles</option>
+							<option value="370">Fishball Noodle (Dry)</option>
+							<option value="411">Wanton Noodle</option>
 							<option value="313">Laksa</option>
 							<option value="284">Ban Mian</option>
-							<option value="461">Prawn Noodles (dry)</option>
-							<option value="294">Prawn Noodles (soup)</option>
-							<option value="373">Fishball Noodles (soup)</option>
+							<option value="461">Prawn Noodle (Dry)</option>
+							<option value="294">Prawn Noodle (Soup)</option>
+							<option value="373">Fishball Noodle (Soup)</option>
 							<option value="535">Chicken Burger</option>
 							<option value="407">Fish Burger</option>
 							<option value="129">Mushroom Soup</option>
 							<option value="504">Spaghetti Bolognese</option>
 							<option value="460">Carbonara</option>
 							<option value="681">Fish and Chips</option>
-							<option value="486">Aglio Olio</option>
+							<option value="486">Agli Olio</option>
+							<option value="555">Mee Rebus</option>
+							<option value="520">Mee Siam</option>
+							<option value="520">Mee Soto</option>
+							<option value="312">Maggie Goreng</option>
+							<option value="159">Nasi Goreng</option>
+							<option value="339">Nasi Lemak</option>
+							<option value="466">Lontong</option>
+							<option value="430">Teriyaki Chicken with Rice</option>
+							<option value="560">Teriyaki Salmon with Rice</option>
+							<option value="698">Omurice</option>
+							<option value="513">Chicken Ramen</option>
+							<option value="855">Katsu Pork with Rice</option>
+							<option value="1103">Katsu Chicken with Rice</option>
+							<option value="182">Chicken Porridge</option>
+							<option value="466">White Carrot Cake</option>
+							<option value="199">Chicken Macaroni</option>
+							<option value="493">Black Carrot Cake</option>
+							<option value="302">Watercress Soup with Rice</option>
+							<option value="232">ABC Soup with Rice</option>
+							<option value="250">Fried Bee Hoon</option>
+							<option value="500">Fried Hokkien mee</option>
+							<option value="380">Yong Tau Foo with Rice </option>
+							<option value="510">Fried Kway Teow</option>
 						</select>
 					</div>
 				</div>
